e368f91d07d3c97d5a8b23fe000ffc8aa0bd0b04
huangpeng <huangpeng@moqipobing.com>
2019-02-28 12:40:00 +0800

    威威

---
+++ src/index.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="wrapper">
+    <image :src="logo" class="logo" />
+    <text class="greeting">The environment is ready!</text>
+    <div @click='useAndroidApi'>调用android原声api</div>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data () {
+    return {
+      logo: 'https://gw.alicdn.com/tfs/TB1yopEdgoQMeJjy1XaXXcSsFXa-640-302.png'
+    }
+  },
+  methods: {
+    useAndroidApi () {
+      weex.requireModule('event').printLog('男哥无敌')
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .wrapper {
+    justify-content: center;
+    align-items: center;
+  }
+  .logo {
+    width: 424px;
+    height: 200px;
+  }
+  .greeting {
+    text-align: center;
+    margin-top: 70px;
+    font-size: 50px;
+    color: #41B883;
+  }
+  .message {
+    margin: 30px;
+    font-size: 32px;
+    color: #727272;
+  }
+</style>
